70ee54f9e532974286526c0b4021187bd9970c86
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2023-05-01 00:27:48 +0300

    Add bi-lstm for sentiment analysis

---
+++ NLP/Sentiment Analysis/dataset.py
@@ -0,0 +1,25 @@
+import torch
+
+class IMDBDataset:
+    def __init__(self, reviews, targets):
+        """
+        Argument:
+        reviews: a numpy array
+        targets: a vector array
+        
+        Return xtrain and ylabel in torch tensor datatype, stored in dictionary format
+        """
+        self.reviews = reviews
+        self.target = targets
+    
+    def __len__(self):
+        # return length of dataset
+        return len(self.reviews)
+    
+    def __getitem__(self, index):
+        # given an index (item), return review and target of that index in torch tensor
+        review = torch.tensor(self.reviews[index,:], dtype = torch.long)
+        target = torch.tensor(self.target[index], dtype = torch.float)
+        
+        return {'review': review,
+                'target': target}
